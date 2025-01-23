f071a876bf5bd497e8ff331659ea2bb0d53147a1
Lam Nguyen <lamnguyendnvncn@gmail.com>
2025-01-22 22:30:27 -0500

    ignore the build files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    ignores: ["dist/**"],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];

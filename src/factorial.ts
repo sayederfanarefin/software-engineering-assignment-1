052624da1f52843647bd08ad997ca5d972be156e
Sayed Erfan Arefin <erfanjordison@gmail.com>
2025-02-03 11:40:25 -0500

    updates

--- src/factorial.ts
+++ src/factorial.ts
@@ -5,7 +5,7 @@ export default function factorial(n: number): number {
   if (n < 0) {
     return -1;
   } else if (n === 0 || n === 1) {
-    return 1;
+    return -1;
   }
   return n * factorial(n - 1);
 }
